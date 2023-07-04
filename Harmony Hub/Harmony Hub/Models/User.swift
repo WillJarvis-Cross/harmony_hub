3b60981faec4a71ea267b3079e9f06b3851cec47
WillJarvis-Cross <72054183+WillJarvis-Cross@users.noreply.github.com>
2023-07-03 21:43:26 -0400

    Add Palette

---
+++ Harmony Hub/Harmony Hub/Models/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  Harmony Hub
+//
+//  Created by Will Jarvis-Cross on 2023-07-03.
+//
+
+import Foundation
+
+struct User {
+    var username: String
+    var palette = "main"
+    //
+    var allColours = {
+        "main"; ["Primary", "PrimaryLight", "Secondary", "Tertiary"]
+    }
+    
+}
